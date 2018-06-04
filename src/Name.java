69937a534f5ad816867668b84db3a9be6d65ee28
Mihau95 <misiek0019@wp.pl>
2018-06-04 22:34:00 +0200

    Zadanie 5.1

---
+++ src/Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    public static void main(String[] args) {
+        String [] = {"Wojtek", "Arek", "Anna"};
+
+        for (int n=0; n<name.length; n++){
+            System.out.println(name[n]);
+        }
+    }
+}
